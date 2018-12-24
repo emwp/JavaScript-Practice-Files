dfb29c6d21c91e76c1503bcffb496a636bc29593
Everton Pereira <everton.mwp@gmail.com>
2018-12-23 22:28:54 -0200

    updated HTTP API Lib to use promises

---
+++ EasyHTTP/app2.js
@@ -0,0 +1,29 @@
+const http = new EasyHTTP;
+
+// Get users 
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// USER DATA
+const data = {
+  name: 'Michael Scott',
+  username: 'MichaelScarn',
+  email: 'mscott@dundermifflin.com'
+}
+
+// CREATE USER
+  // http.post('https://jsonplaceholder.typicode.com/users', data)
+  // .then(data => console.log(data))
+  // .catch(err => console.log(err));
+
+// CREATE PUT // UPDATE USER
+  // http.put('https://jsonplaceholder.typicode.com/users/2', data)
+  // .then(data => console.log(data))
+  // .catch(err => console.log(err));  
+
+
+// DELETE USER
+  http.delete('https://jsonplaceholder.typicode.com/users/2')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
